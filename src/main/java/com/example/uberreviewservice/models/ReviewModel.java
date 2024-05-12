50fabee09976b3b6e049ec548df66b22cab850d5
srinibas khuntia <srinibaskhuntia39@gmail.com>
2024-05-12 05:46:35 -0700

    implemented inheritance using Base Model and MappedSuperClass

--- src/main/java/com/example/uberreviewservice/models/ReviewModel.java
+++ src/main/java/com/example/uberreviewservice/models/ReviewModel.java
@@ -16,25 +16,25 @@ import java.util.Date;
 @EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name="bookingReview") // name of the table
-public class ReviewModel {
+public class ReviewModel extends BaseModel {
 
-    @Id //making id as primary key
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
-    Long id;
+//    @Id //making id as primary key
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
+//    Long id;
 
     @Column(nullable = false)
     private String content;
 
     private Double Rating;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(nullable = false)
-    @CreatedDate //timeestamp of created object
-    private Date createdAt;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate //timestamp of updated Object
-    private Date updatedAt;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @Column(nullable = false)
+//    @CreatedDate //timeestamp of created object
+//    private Date createdAt;
+//
+//    @Column(nullable = false)
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @LastModifiedDate //timestamp of updated Object
+//    private Date updatedAt;
 
 }
